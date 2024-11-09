8abfd8d17b972e2570fc9b9da0345614c7f4b379
MahidunNobi <mdmahidunnobi@gmail.com>
2024-11-09 08:54:31 +0600

    Solved: problem-03

---
+++ src/problem-3.ts
@@ -0,0 +1,10 @@
+
+const countWordOccurrences= (sen:string, word:string)=>{
+    const filterRegex = new RegExp(word, "i")
+    const wordOccarence = sen.split(" ").filter(w => filterRegex.test(w)).length
+    return wordOccarence
+}
+
+
+
+countWordOccurrences("I love typescript", "typescript");
